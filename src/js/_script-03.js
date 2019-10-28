89a0e0a987016c92cf2fd55c26539421bec8e062
Anna Butrimenko <annettt@gmail.com>
2019-10-28 15:54:52 +0200

    Add doctors form class

--- src/js/_script-03.js
+++ src/js/_script-03.js
@@ -20,33 +20,84 @@ class Form {
         this._form.action = this._action;
         this._form.addEventListener("submit", (e) => {
             e.preventDefault();
-            // const data = .serialize();
+
             const data =  this.serialize();
-            // const values = data.map(item => item.value);
             console.log(data);
-            const visit = new Visit(data);
-            this._form.closest(".modal-overlay").classList.add('is-hidden');
+
+                this._form.closest(".modal-overlay").classList.add('is-hidden');
+            // if (e.target.id === 'visit-modal') {
+            //     let test = document.getElementById('select-doctors');
+            //     // console.log(test);
+            //     // console.log(e.target.id);
+            //     const visit = new Visit(data);
+            //     // console.log(visit);
+            // visit.render(document.querySelector('.cards-desk'))
+            //
+            // }
+
+
             // const visit = new Visit("fggh", 'efgg', 'adf', 'eggrg', 'wegw');
             // debugger;
             // console.log(data);
-            console.log(visit);
             // console.log(this.serialize(visit));
-            visit.render(document.querySelector('.cards-desk'))
         })
 
             container.appendChild(this._form);
     }
     serialize() {
-        const inputs = this._form.querySelectorAll("input");
+        const inputs = this._form.querySelectorAll("input:not([type='submit'])");
         const arr = [...inputs].filter(item => item.value);
+        console.log(arr);
         const obj = {};
-        const arrObj = arr.forEach(input => {
+        arr.forEach(input => {
             obj[input.name] = input.value;
             });
         return obj;
     }
 
 
+}
+
+class CardioForm extends Form {
+    constructor (...args) {
+        super(...args);
+    }
+    render(container) {
+        super.render(container);
+        const visitPurpose = new Input('', 'Enter purpose of visit', 'visit-purpose');
+        const description = new Input('text', 'Description', 'description');
+        const selectCardio = new Select('cardio-select', '', 'Ivanov', 'Petrov', 'Sidorov');
+        const selectPriority = new Select('priority-select', '', 'Low', 'Medium', 'High');
+        const bloodPressure = new Input ('text', 'Pressure', 'pressure');
+        const weightIndex = new Input('number', 'Index', 'weight-index');
+        const diseases = new Input('','Diseases', 'diseases');
+        const age = new Input('number','Age', 'age');
+        const fullName = new Input('','Full name', 'full-name');
+
+        // const titleCard = new Input('text', 'title', 'title');
+        // const patientName = new Input('text', 'patient name', 'name');
+        // const status = new Input('text', 'Status', 'status');
+        // const priority = new Input('text', 'Priority', 'priority');
+        const cardSubmit = new Input('submit', '', '', 'Отправить');
+
+        visitPurpose.render(this._form);
+        description.render(this._form);
+        selectCardio.render(this._form);
+        selectPriority.render(this._form);
+        bloodPressure.render(this._form);
+        weightIndex.render(this._form);
+        diseases.render(this._form);
+        age.render(this._form);
+        fullName.render(this._form);
+
+        // titleCard.render(this._form);
+        // patientName.render(this._form);
+        // status.render(this._form);
+        // priority.render(this._form);
+        cardSubmit.render(this._form);
+
+
+    }
 }
 // const testForm = new Form('GET', 'testik');
 // const form1 = document.getElementById('test-form');
--- src/js/_script-05.js
+++ src/js/_script-05.js
@@ -22,23 +22,55 @@ submitInput.render(document.getElementById('login-form'));
 const addCardBtn = document.querySelector('.add-card-btn');
 addCardBtn.openModal = modalCard.open.bind(modalCard);
 
-const formCard = new Form("POST", 'visit-modal');
+const formCard = new Form("POST", 'visit-modal', 'class-cardio');
 formCard.render(document.querySelector('#card-modal'));
 
-const titleCard = new Input('text', 'title', 'title');
-const patientName = new Input('text', 'patient name', 'name');
-const description = new Input('text', 'Description', 'description');
-const status = new Input('text', 'Status', 'status');
-const priority = new Input('text', 'Priority', 'priority');
-const cardSubmit = new Input('submit', '', '', 'Отправить');
+const select = new Select('add', 'select-doctors',  '-select doctor-', 'Cardio', 'Dantist', 'Therapist');
+
+// const titleCard = new Input('text', 'title', 'title');
+// const patientName = new Input('text', 'patient name', 'name');
+// const description = new Input('text', 'Description', 'description');
+// const status = new Input('text', 'Status', 'status');
+// const priority = new Input('text', 'Priority', 'priority');
+// const cardSubmit = new Input('submit', '', '', 'Отправить');
 
 
-titleCard.render(document.getElementById('visit-modal'));
-patientName.render(document.getElementById('visit-modal'));
-description.render(document.getElementById('visit-modal'));
-status.render(document.getElementById('visit-modal'));
-priority.render(document.getElementById('visit-modal'));
-cardSubmit.render(document.getElementById('visit-modal'));
+select.render(document.getElementById('visit-modal'));
+let selectDoctor = document.getElementById('select-doctors');
+selectDoctor.addEventListener('change', function () {
+  const removeForm = document.getElementsByClassName('doctor-select-form');
+  const doctor = selectDoctor.options[selectDoctor.selectedIndex].value;
+  if(removeForm.length === 1) {
+      removeForm[0].remove();
+  } else {
+    // if (doctor === 'Cardio') {
+    //   const cardioForm = new CardioForm('POST', 'id2','cardio-form');
+    //   cardioForm.render(document.getElementById('visit-modal'))
+    // }
+    switch (doctor) {
+      case 'Cardio':
+        const cardioForm = new CardioForm('POST', '','doctor-select-form');
+        cardioForm.render(document.getElementById('visit-modal'))
+        break;
+      case 'Dantist':
+        const dantistForm = new CardioForm('POST', '','doctor-select-form');
+        cardioForm.render(document.getElementById('visit-modal'))
+        break;
+      case 'Therapist':
+        const therapistForm = new CardioForm('POST', '','doctor-select-form');
+        cardioForm.render(document.getElementById('visit-modal'))
+        break;
+    }
+
+  }
+
+});
+// titleCard.render(document.getElementById('visit-modal'));
+// patientName.render(document.getElementById('visit-modal'));
+// description.render(document.getElementById('visit-modal'));
+// status.render(document.getElementById('visit-modal'));
+// priority.render(document.getElementById('visit-modal'));
+// cardSubmit.render(document.getElementById('visit-modal'));
 
 
 
